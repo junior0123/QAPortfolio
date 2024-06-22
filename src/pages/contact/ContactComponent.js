18bc87d5bd866584d4dd771526a154e8f06bb517
Alvaro Sivila <juniorsivila123@gmail.com>
2024-06-22 16:30:16 -0400

    COmmented code for contact me section

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -55,6 +55,7 @@ class Contact extends Component {
               </div>
             </div>
           </Fade>
+          {/* 
           <Fade bottom duration={1000} distance="40px">
             <div className="blog-heading-div">
               <div className="blog-heading-text-div">
@@ -80,7 +81,7 @@ class Contact extends Component {
                 {/* <img
 											src={require(`../../assests/images/${blogSection["avatar_image_path"]}`)}
 											alt=""
-										/> */}
+										/> *}
                 <BlogsImg theme={theme} />
               </div>
             </div>
@@ -91,7 +92,7 @@ class Contact extends Component {
                 {/* <img
 											src={require(`../../assests/images/${addressSection["avatar_image_path"]}`)}
 											alt=""
-										/> */}
+										/> *}
                 <AddressImg theme={theme} />
               </div>
               <div className="address-heading-text-div">
@@ -130,6 +131,7 @@ class Contact extends Component {
               </div>
             </div>
           </Fade>
+          */}
         </div>
         <Footer theme={this.props.theme} onToggle={this.props.onToggle} />
         <TopButton theme={this.props.theme} />
